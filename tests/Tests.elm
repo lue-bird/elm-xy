7266624907fa9641c8df8f73d81d8b35a789129b
passi <pascalfeyer@gmail.com>
2021-06-15 21:03:58 +0200

    + normalize & toAngle tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,7 +2,7 @@ module Tests exposing (suite)
 
 import Expect exposing (FloatingPointTolerance(..))
 import Test exposing (Test, describe, test)
-import Xy exposing (Xy, xy)
+import Xy exposing (Xy, x, xy, y)
 
 
 suite : Test
@@ -11,6 +11,28 @@ suite =
         [ test "length"
             (\() ->
                 Xy.length (xy 3 4)
-                    |> Expect.within (Absolute 0.00001) 5
+                    |> expectEqualFloat 5
+            )
+        , test "normalize"
+            (\() ->
+                Xy.normalize (xy 3 4)
+                    |> Expect.all
+                        [ x >> expectEqualFloat (3 / 5)
+                        , y >> expectEqualFloat (4 / 5)
+                        ]
+            )
+        , test "toAngle"
+            (\() ->
+                Expect.all
+                    [ \_ -> Xy.toAngle ( 1, 1 ) |> expectEqualFloat (turns (1 / 8))
+                    , \_ -> Xy.toAngle ( -1, 1 ) |> expectEqualFloat (turns (3 / 8))
+                    , \_ -> Xy.toAngle ( -1, -1 ) |> expectEqualFloat -(turns (3 / 8))
+                    , \_ -> Xy.toAngle ( 1, -1 ) |> expectEqualFloat -(turns (1 / 8))
+                    ]
+                    ()
             )
         ]
+
+
+expectEqualFloat =
+    Expect.within (Absolute 0.000001)
